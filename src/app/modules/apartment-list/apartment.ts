735a07df8e087ff577f9e56fdf6f98a102af8953
Piotr <peter.lubowicz@gmail.com>
2024-01-07 14:06:33 +0100

    add address to apartment list

--- src/app/modules/apartment-list/apartment.ts
+++ src/app/modules/apartment-list/apartment.ts
@@ -1,8 +1,12 @@
 import { DTRow } from "src/app/shared/data-table/DTRow"
 
 export interface Apartment extends DTRow {
-    id: number,
-    apartmentName: string,
+    id: number
+    apartmentName: string
     leasesNumber: number
     isRented: boolean
+    cityName: string
+    streetName: string
+    buildingNumber: string
+    apartmentNumber: string
 }
