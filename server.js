a8e33196c3b5ed19bcac2fa14263dc4874466e2e
Piotr <peter.lubowicz@gmail.com>
2024-02-21 21:04:01 +0100

    ass server js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const proxy = require('http-proxy-middleware');
+const app = express();
+
+const apiProxy = proxy.createProxyMiddleware("/api", {
+    target: process.env.BACKEND_URL || '',
+    changeOrigin: true
+});
+app.use(apiProxy);
+app.use(express.static(__dirname + "/dist/rentowne"));
+
+app.get("/*", (req, res) => {
+    res.sendFile(__dirname + "/dist/rentowne/index.html")
+});
+
+app.listen(process.env.PORT || 3000);
